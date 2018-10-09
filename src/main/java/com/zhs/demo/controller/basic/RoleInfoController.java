7e947be317a431fbd03d7f61d8dda8bf738d6303
HS-Z <www.aiwoo@live.com>
2018-10-09 13:39:45 +0800

    日志

--- src/main/java/com/zhs/demo/controller/basic/RoleInfoController.java
+++ src/main/java/com/zhs/demo/controller/basic/RoleInfoController.java
@@ -9,6 +9,8 @@ import com.zhs.demo.service.basic.RoleInfoService;
 import com.zhs.demo.service.notice.EmailService;
 import com.zhs.demo.utils.Json;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,6 +22,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("roleInfo")
 public class RoleInfoController {
+
+    private Logger logger= LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     private RoleInfoService roleInfoService;
